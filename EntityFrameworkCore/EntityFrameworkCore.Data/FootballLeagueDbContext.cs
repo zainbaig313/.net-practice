cc2911843ac919d8d8f81c1e416d562ec6ee6fd6
zainbaig313 <baigzain480@gmail.com>
2026-09-24 22:10:31 +0500

    learning migration of database

--- EntityFrameworkCore/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -6,12 +6,19 @@ namespace EntityFramworkCore.Data
 {
     public class FootballLeagueDbContext : DbContext
     {
+
+        private string Dbpath;
+        public FootballLeagueDbContext()
+        {
+         var path = Directory.GetCurrentDirectory();
+          Dbpath = Path.Combine(path, "FootballLeague_EfCore.db");   
+        }
         public DbSet<Team> Teams {get;set;}
         public DbSet<Coach> Coaches {get; set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source=FootballLeague_EfCore.db");
+            optionsBuilder.UseSqlite($"Data Source={Dbpath}");
         }
     }
 }
